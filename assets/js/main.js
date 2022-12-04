20b11a8d070ec0a4f0b22046689a99ba171c6490
Abdalrhman M. Alkady <alkady2019@gmail.com>
2022-12-04 03:44:02 +0200

    start

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+(function($){
+	"use strict";
+	// Go to Top
+	$(function(){
+		// Scroll Event
+		$(window).on('scroll', function(){
+			var scrolled = $(window).scrollTop();
+			if (scrolled > 600) $('.go-top').addClass('active');
+			if (scrolled < 600) $('.go-top').removeClass('active');
+		});  
+		// Click Event
+		$('.go-top').on('click', function() {
+			$("html, body").animate({ scrollTop: "0" },  500);
+		});
+	});
+
+}(jQuery));
+
+// PreLoader
+window.onload = function(){
+    var el = document.getElementById('preloader');
+    el.style.display = 'none';
+  };
